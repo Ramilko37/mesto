da7709a8cc98e6c8b6235497051305271902be84
Ramilko37 <galyamdin@gmail.com>
2020-11-19 18:57:37 +0300

    delau mergelease enter the commit message for your changes. Lines starting

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,7 +65,7 @@ api.getContent()
 
         cardList.renderItems()
 
-        function addNewCard(data) {
+        function createCard(data) {
             const card = new Card(
                 data,
                 '.card-template',
@@ -104,9 +104,17 @@ api.getContent()
                     }
                 }
             );
-            const cardElement = card.createCard();
-            cardList.addItem(cardElement);
+           return card.createCard();
+            
         }
+
+        function addNewCard(data) {
+            const card = createCard(data);
+            cardList.addItem(card);
+        }
+
+
+
         console.log('userInfoData avatar', userInfoData)
         // обновить профиль юзера
         userInfo.setUserInfo({
